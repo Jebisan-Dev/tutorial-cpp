913c3f187b4de064e706ff93cad74011f85e0fd0
jebianofficial@gmail.com <jebisanofficial@gmail.com>
2025-04-04 19:14:10 +0530

    feat: added a new problem solution code

--- problems/highest_num.cpp
+++ problems/highest_num.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int num1, num2, num3;
+    int num1, num2, num3, high;
 
     cout << "1st number: ";
     cin >> num1;
@@ -12,4 +12,17 @@ int main()
     cin >> num2;
     cout << "3rd number: ";
     cin >> num3;
+
+    if (high == num1 > num2 && num1 > num2)
+    {
+        cout << num1 << " is the highest number" << endl;
+    }
+    else if (high == num2 > num1 && num2 > num3)
+    {
+        cout << num2 << " is the highest number" << endl;
+    }
+    else
+    {
+        cout << num3 << " is the highest number" << endl;
+    }
 }
